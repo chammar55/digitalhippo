08e572317d5b4da1c2a1e157e281b8ba336cac5c
Muhammad Ammar Ibrahim <96951639+chammar55@users.noreply.github.com>
2024-07-11 09:26:48 +0500

    completed till 33 min

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    // cn function help us to apply the styles that we want should be default that must be applied and className styles that we want to change according to diff pages
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
